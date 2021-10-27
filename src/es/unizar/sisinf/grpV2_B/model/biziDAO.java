7fbb204a71a73bd9b1cbff23a1db160398aa2614
JorgeGrima <91877348+JorgeGrima@users.noreply.github.com>
2021-10-27 21:16:35 +0200

    Update biziDAO.java

--- src/es/unizar/sisinf/grpV2_B/model/biziDAO.java
+++ src/es/unizar/sisinf/grpV2_B/model/biziDAO.java
@@ -8,11 +8,68 @@ import es.unizar.sisinf.grpV2_B.db.*;
 
 public class biziDAO {
 
-    private static String ltiasEstaciones = "SELECT * FROM BiziStation ORDER BY id ASC";
+    private static String lsEstaciones = "SELECT * FROM BiziStation ORDER BY id ASC";
+    
+    public List<biziVO> listar(){
+
+		List<biziVO> listaEstaciones = new LinkedList<biziVO>();
+        Connection conn = null;
+
+		try {
+			conn = ConnectionManager.getConnection();
+            conn.getConnection();
+            PreparedStatement lsEst = conn.PreparedStatement(lsEstaciones);
+
+            ResultSet rs = lsEst.executeQuery();
+
+            while(rs.next()){
+                biziVO estacion = new biziVO(rs.getInt("id")
+                                                    ,rs.getInt("capacity")
+                                                    ,rs.getInt("available")
+                                                    ,rs.getString("direction")
+                                                    ,rs.getString("localitation")); // es localization :D
+                listaParadas.add(parada);
+            }
+            rs.close();
+            lsEst.close();
+
+		} catch(SQLException se) {
+			se.printStackTrace();  
+		
+		} catch(Exception e) {
+			e.printStackTrace(System.err); 
+		} finally {
+			ConnectionManager.releaseConnection(conn); 
+		}
+		
+		return listaEstaciones;
+    }
+    
+    public void anyadir(biziVO estacion){
+        Connection conn = null;
+
+        try {
+            conn = ConnectionManager.getConnection();
+            conn.getConnection();
+            PreparedStatement lsEst = conn.PreparedStatement(insert);
+            ResultSet rs = lsEst.executeQuery();
+
+            lsEst.setString(3, estacion.getID(), estacion.getCapacidad(), estacion.getBicis(), estacion.getDireccion(), estacion.getLocalizacion());
+            lsEst.executeUpdate();
+
+            rs.close();
+            lsEst.close();
+
+        } catch(SQLException se) {
+            se.printStackTrace();  
+        
+        } catch(Exception e) {
+            e.printStackTrace(System.err); 
+        } finally {
+            ConnectionManager.releaseConnection(conn); 
+        }
+    }
 }
 
 
 // Dado número de estacion -> nbicis disponibles (API)
-// Lista de todas las estaciones
-
-// Introducir nueva estacion en la DB
